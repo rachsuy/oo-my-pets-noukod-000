8524f951770f66940c7cf5c515d8f64db3d2c687
Rachele Suy <rachelesuy@gmail.com>
2019-05-20 20:08:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,20 +33,38 @@ class Owner
   def say_species
     puts "I'm #{species}"
   end
+ 
+  def pets
+    @@pets
+  end
+
+  def buy_fish(fish)
+    @@pets[:fishes] << Fish.new(fish)
+  end
+
   def buy_cat(cat)
     @@pets[:cats] << Cat.new(cat)
   end
-  
-  
-  def buy_fish(cat)
-    @@pets[:fishes] << Fish.new(cat)
-  end
-  
-  
+
   def buy_dog(dog)
-    @@pets[:Dogs] << Dog.new(cat)
+    @@pets[:dogs] << Dog.new(dog)
   end
-   
-  
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def play_with_cats
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+  def feed_fish
+    @@pets[:fishes].each do |fish|
+      fish.mood = 'happy'
+    end
  
 end
