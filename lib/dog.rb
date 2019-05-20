0eccbba76982dc35e0713aa9d13179651937d8d3
Rachele Suy <rachelesuy@gmail.com>
2019-05-20 20:57:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
   # code goes here
+   attr_accessor :dog, :mood
+  attr_reader :name 
+  
+  def initialize(name)
+    @dog = @dog
+    @name =name
+    @mood = "nervous"
+    
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
 class Fish
   # code goes here
+  
+   attr_accessor :fish, :mood
+  attr_reader :name 
+  
+  def initialize(name)
+    @fish = @fish 
+    @name =name
+    @mood = "nervous"
+    
+  end
 end
